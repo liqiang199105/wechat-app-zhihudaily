83683716a6854cc6895159db5a69104e75dd90bc
Larkin <1251976772@qq.com>
2017-03-31 16:49:50 +0800

    add

--- service/s_home.js
+++ service/s_home.js
@@ -60,16 +60,16 @@ var knowledges = {
     "data": [
         {
             "id": 1,
-            "title": "什么是股票",
-            "cover": "https://img.cdn.icaibei.net/avatar/201703/af23cbbcb7362e27f8c2ca76ee21a24c.png",
-            "desc": "股票（stock）是股份公司发行的所有权凭证，是股份公司为筹集资金而发行给各个股东作为持股凭证并借以取得股息和红利的一种有价证券。每股股票都代表股东对企业拥有一个基本单位的所有权。每支股票背后都有一家上市公司。同时，每家上市公司都会发行股票的。",
+            "title": "股票",
+            "cover": "https://img.cdn.icaibei.net/avatar/201703/25ea59394c5537e268b6eb2f6bf02df3.png",
+            "desc": "股票（stock）是股份公司发行的所有权凭证，是股份公司为筹集资金而发行给各个股东作为持股凭证并借以取得股息和红利的一种有价证券。",
             "count": "1"
         },
         {
             "id": 2,
-            "title": "什么是印花税",
-            "cover": "https://img.cdn.icaibei.net/avatar/201703/af23cbbcb7362e27f8c2ca76ee21a24c.png",
-            "desc": "净资产收益率ROE(Rate of Return on Common Stockholders’ Equity)，净资产收益率又称股东权益报酬率/净值报酬率/权益报酬率/权益利润率/净资产利润率，是净利润与平均股东权益的百分比，是公司税后利润除以净资产得到的百分比率，该指标反映股东权益的收益水平，用以衡量公司运用自有资本的效率。指标值越高，说明投资带来的收益越高。该指标体现了自有资本获得净收益的能力。",
+            "title": "印花税",
+            "cover": "https://img.cdn.icaibei.net/avatar/201703/4be487cd30336cf22d20afba1fbad03a.png",
+            "desc": "印花税是对经济活动和经济交往中设立、领受具有法律效力的凭证的行为所征收的一种税。因采用在应税凭证上粘贴印花税票作为完税的标志而得名。",
             "count": "1"
         }
     ]
